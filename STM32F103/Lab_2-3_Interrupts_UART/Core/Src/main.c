b742a590b496e024cd6bfe3d498006400f6a3f1a
Andrey Serebrennikov <seradya@gmail.com>
2023-11-11 13:02:47 +0300

    Commands

--- STM32F103/Lab_2-3_Interrupts_UART/Core/Src/main.c
+++ STM32F103/Lab_2-3_Interrupts_UART/Core/Src/main.c
@@ -8,6 +8,7 @@
 bool CommandReceived;
 char RxBuffer[256];
 char TxBuffer[256];
+uint8_t angle;
 
 int main(void)
 {
@@ -18,6 +19,9 @@ int main(void)
   tim2_init();   // initializing TIM2
   btn_init();    // User button initializingd
   usart_init();  // USART2 initialization
+  led_init_pwm();
+  tim3_pwm_init();
+  led_init_pwm();
 
   while (true)
   {
@@ -37,21 +41,25 @@ void ExecuteCommand(void)
     strcpy(TxBuffer,"Serebrennikov AN\n\rDobrotinAA\n\rIU4-71B\n\r");
   }
 
-  else if (strncmp(RxBuffer, "Start", 5) == 0)
+  else if (strncmp(RxBuffer, "LEFT", 4) == 0)
   {
-    TIM2->CR1 |= TIM_CR1_CEN;
-    strcpy(TxBuffer, "Ok, start");
+    
+    strcpy(TxBuffer, "Ok, LEFT");
+    tim3_set_angle(180);
+    angle = 180;
   }
 
-  else if (strncmp(RxBuffer, "Stop", 4) == 0)
+  else if (strncmp(RxBuffer, "RIGHT", 5) == 0)
   {
-    TIM2->CR1 &= ~TIM_CR1_CEN;
-    strcpy(TxBuffer, "Ok, stop");
+    
+    strcpy(TxBuffer, "Ok, RIGHT");
+    tim3_set_angle(0);
+    angle = 0;
   }
 
-  else if (strncmp(RxBuffer, "ANG", 3) == 0)
+  else if (strncmp(RxBuffer, "ANG ", 4) == 0)
   {
-    uint8_t ang;
+    uint16_t ang;
     sscanf(RxBuffer, "%*s %hu", &ang);
     if (ang > 180)
     {
@@ -59,9 +67,18 @@ void ExecuteCommand(void)
     }
     else
     {
-      void tim3_set_angle(ang);
-      strcpy(TxBuffer, "Angle %d is set.", ang);
+      tim3_set_angle((uint8_t)ang);
+      angle = ang;
+      strcpy(TxBuffer, "Angle is set.");
     }
+  }
+  else if (strncmp(RxBuffer, "ANG?", 4) == 0)
+  {
+    char str[3];
+    sprintf(str, "%d", angle);
+    strcpy(TxBuffer, "ANG:");
+    strcat(TxBuffer, str);
+    
   }
 
   // else if(strncmp(RxBuffer, "Period", 6) == 0)
